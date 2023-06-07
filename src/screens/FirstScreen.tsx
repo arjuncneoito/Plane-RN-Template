e530935ef51998df2861d8e102610d3aef8b76a7
arjuncneoito <arjun.cr@neoito.com>
2023-06-07 15:48:09 +0530

    feat:'first commit'

--- src/screens/FirstScreen.tsx
+++ src/screens/FirstScreen.tsx
@@ -1,12 +1,33 @@
-import {Text, View} from 'react-native';
-import React from 'react';
+import {Appearance, Button, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {useTheme} from '../hooks/DarkMode/Dartheme';
+import appSlice, {Theme} from '../redux/slices/app.slice';
+import {useDispatch} from 'react-redux';
 
 const FirstScreen = () => {
+  const {theme, changeTheme} = useTheme();
+  const dispatch = useDispatch();
+  const handleAppearanceChange = () => {
+    const colorScheme: Theme = Appearance.getColorScheme() ?? 'light';
+    dispatch(appSlice.actions.setTheme(colorScheme));
+  };
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener(handleAppearanceChange);
+    return () => {
+      subscription.remove();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <View>
+    <View style={[styles.container, theme.backgroundColor]}>
+      <Button title="Press me" onPress={() => changeTheme('dark')} />
       <Text>FirstScreen</Text>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {flex: 1},
+});
 
 export default FirstScreen;
--- src/screens/SecondScreen.tsx
+++ src/screens/SecondScreen.tsx
@@ -1,12 +1,19 @@
-import {Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useTheme} from '../hooks/DarkMode/Dartheme';
 
 const SecondScreen = () => {
+  const {theme, changeTheme} = useTheme();
+
   return (
-    <View>
+    <View style={[styles.container, theme.backgroundColor]}>
+      <Button title="Press me" onPress={() => changeTheme('light')} />
+
       <Text>SecondScreen</Text>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {flex: 1},
+});
 export default SecondScreen;
